10ea732971928a2e203f3682ae81c99dccfe4b27
Eric König <120642443+Sonrotz0815@users.noreply.github.com>
2024-11-22 15:00:20 +0100

    Create Base Files for voucher connection

---
+++ src/vouchers/entities/voucher.entity.ts
@@ -0,0 +1,35 @@
+/* eslint-disable */
+// src/users/entity/users.entity.ts
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+ 
+@Entity("Vouchers") // Declares the class as an entity
+export class Voucher {
+  @PrimaryGeneratedColumn() // Auto-incremented primary key
+  Id: number;
+ 
+  @Column() // Specifies a regular column
+  VoucherCode: string;
+ 
+  @Column() // Specifies a regular column
+  ValidatedOn: Date;
+ 
+  @Column() // Specifies a regular column
+  WorkspaceId: string;
+ 
+  @Column() // Specifies a regular column
+  WinningChallengeId: string;
+ 
+  @Column()
+  CreatedById : string;
+
+  @Column()
+  LastModifiedById : string;
+
+  @Column()
+  LastModifiedOn : Date;
+  
+  @Column()
+  SubmissionId : string;
+  
+
+}
